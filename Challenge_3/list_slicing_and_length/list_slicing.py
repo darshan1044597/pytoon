c5dfe49941abf5daf6afa7e85b67d479e33b164e
darshan1044597 <1044597@pdsb.net>
2025-03-21 10:50:37 -0400

    added new files and code for challenge 3

---
+++ Challenge_3/list_slicing_and_length/list_slicing.py
@@ -0,0 +1,5 @@
+print("welcome to the colors list")
+colors = ['red', 'blue', 'green', 'yellow', 'purple']
+print(colors)
+print("sliced the list for only the second and third colors:")
+print(colors[1:3])
