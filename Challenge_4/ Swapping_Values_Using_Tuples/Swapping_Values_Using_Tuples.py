d91c93800b0ceb81601b3c2dec8e4fba1d3ef8cf
darshan1044597 <1044597@pdsb.net>
2025-03-28 08:36:02 -0400

    added new files and code for challenge 4 tuples

--- Challenge_4/ Swapping_Values_Using_Tuples/Swapping_Values_Using_Tuples.py
+++ Challenge_4/ Swapping_Values_Using_Tuples/Swapping_Values_Using_Tuples.py
@@ -8,11 +8,13 @@
 # Updated:     27-Mar-2025
 #-----------------------------------------------------------------------------
 # Print a welcome message
-print("Welcome to swapping values using tuples")
+print("Welcome to swapping values using tuples, here you enter two numbers and the program will swap their places")
 # tell the user to enter the first number and convert the input to an integer
 num1 = int(input("Enter the first number: "))
 # ask the user to enter the second number and convert the input to an integer
 num2 = int(input("Enter the second number: "))
+# make a tuples for num1 and num2
+num1, num2 = (num1, num2)
 # Swap the values of num1 and num2
 num1, num2 = num2, num1
 # Print the swapped values to the user
--- Challenge_4/Tuple_basics/readme.md
+++ Challenge_4/Tuple_basics/readme.md
@@ -1,4 +1,2 @@
-print the entire tuple
-access the third element of the tuple and print it
-extract the fifth element (the nested tuple) from the main tuple
-print the first element of the nested tuple
+### print the entire tuple then access the third element of the tuple and print it, next extract the fifth element (the nested tuple) from the main tuple, and finally print the first element of the nested tuple
+
