354048ebe75458b3a47a27cc6a70214972c0026c
darshan1044597 <1044597@pdsb.net>
2025-04-08 08:49:52 -0400

    added new files and code for challenge 4

--- Challenge_4_Tuples/ Swapping_Values_Using_Tuples/Swapping_Values_Using_Tuples.py
+++ Challenge_4_Tuples/ Swapping_Values_Using_Tuples/Swapping_Values_Using_Tuples.py
@@ -10,12 +10,12 @@
 # Print a welcome message
 print("Welcome to swapping values using tuples, here you enter two numbers and the program will swap their places")
 # tell the user to enter the first number and convert the input to an integer
-num1 = int(input("Enter the first number: "))
+num1 = int(input("Please enter the first number: "))
 # ask the user to enter the second number and convert the input to an integer
-num2 = int(input("Enter the second number: "))
+num2 = int(input("Please enter the second number: "))
 # creates a tuple with the input given
 tuple = (num1, num2)
 # prints a message to say what the swapped values are
-print("the swapped tuple is indexes are:")
+print("The swapped tuple indexes for your numbers are:")
 # swaps the tuple index values
 print(tuple[1], tuple[0])
